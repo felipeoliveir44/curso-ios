3386bba3153a2d1d2ecf5b91fe37118363469da0
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-05-08 21:33:49 -0300

    Ex 2

---
+++ workspace/js/exercicios/ex-2/assets/js/main.js
@@ -0,0 +1,10 @@
+var nota1 = 5;
+var nota2 = 5;
+var nota3 = 2;
+var nota4 = 10;
+
+var resultadoSoma = nota1 + nota2 + nota3 + nota4;
+
+var media = resultadoSoma / 4;
+
+console.log("A média do aluno com as 4 notas é: " + media);
