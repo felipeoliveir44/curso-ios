c6f43cd790cb386c4dbbeef360eaaa89ebe1e40e
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-05-09 21:19:40 -0300

    Ex 3

--- workspace/js/exercicios/ex-3/assets/js/main.js
+++ workspace/js/exercicios/ex-3/assets/js/main.js
@@ -1,8 +1,8 @@
-let fruta1 = ' Laranja';
-let fruta2 = 'Banana';
-let fruta3 = 'Abacaxi';
-let fruta4 = 'Manga';
-let fruta5 = 'Morango';
+const fruta1 = ' Laranja';
+const fruta2 = 'Banana';
+const fruta3 = 'Abacaxi';
+const fruta4 = 'Manga';
+const fruta5 = 'Morango';
 
 // Length
 console.log(`A quantidade de caracteres da fruta 1 é: ${fruta1.length}, fruta 2 é: ${fruta2.length}, fruta 3 é: ${fruta3.length}, fruta 4 é: ${fruta4.length} e fruta 5 é: ${fruta5.length}`);
@@ -14,10 +14,8 @@ console.log(fruta1.substring(0, 3), fruta2.substring(0, 3), fruta3.substring(0, 
 console.log(fruta1.trim());
 
 // Replace
-console.log(fruta1.replace(' Laranja', 'Kiwi'), fruta2.replace('Banana', 'Pêssego'), fruta3.replace('Abacaxi', 'Uva'), fruta4.replace('Manga', 'Melancia'), fruta5.replace('Morango', 'Abacate'));
+console.log(fruta1.replace(' Laranja', 'Kiwi'.toLowerCase()), fruta2.replace('Banana', 'Pêssego'.toLowerCase()), fruta3.replace('Abacaxi', 'Uva'.toLowerCase()), fruta4.replace('Manga', 'Melancia'.toLowerCase()), fruta5.replace('Morango', 'Abacate'.toLowerCase()));
 
-// LowerCase
-console.log(fruta1.toLowerCase(), fruta2.toLowerCase(), fruta3.toLowerCase(), fruta4.toLowerCase(), fruta5.toLowerCase());
 
 // Listagem das frutas
 
