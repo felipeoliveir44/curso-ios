02c3cce94a9b21d5bc7936385190a09b80d4ecf8
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-05-09 21:10:12 -0300

    Ex 3

--- workspace/js/exercicios/ex-3/assets/js/main.js
+++ workspace/js/exercicios/ex-3/assets/js/main.js
@@ -1,4 +1,4 @@
-let fruta1 = '  Laranja';
+let fruta1 = ' Laranja';
 let fruta2 = 'Banana';
 let fruta3 = 'Abacaxi';
 let fruta4 = 'Manga';
@@ -8,16 +8,16 @@ let fruta5 = 'Morango';
 console.log(`A quantidade de caracteres da fruta 1 é: ${fruta1.length}, fruta 2 é: ${fruta2.length}, fruta 3 é: ${fruta3.length}, fruta 4 é: ${fruta4.length} e fruta 5 é: ${fruta5.length}`);
 
 // Substring
-console.log(fruta2.substring(0, 3));
+console.log(fruta1.substring(0, 3), fruta2.substring(0, 3), fruta3.substring(0, 3), fruta4.substring(0, 3), fruta5.substring(0, 3));
 
 // Trim
 console.log(fruta1.trim());
 
 // Replace
-console.log(fruta3.replace('Abacaxi', 'Kiwi'));
+console.log(fruta1.replace(' Laranja', 'Kiwi'), fruta2.replace('Banana', 'Pêssego'), fruta3.replace('Abacaxi', 'Uva'), fruta4.replace('Manga', 'Melancia'), fruta5.replace('Morango', 'Abacate'));
 
 // LowerCase
-console.log(fruta3.toLowerCase());
+console.log(fruta1.toLowerCase(), fruta2.toLowerCase(), fruta3.toLowerCase(), fruta4.toLowerCase(), fruta5.toLowerCase());
 
 // Listagem das frutas
 
