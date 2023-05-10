ab2b3f31a99a4db47b855494b8b190da9cc427f0
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-05-09 21:01:03 -0300

    Ex 3

---
+++ workspace/js/exercicios/ex-3/assets/js/main.js
@@ -0,0 +1,27 @@
+let fruta1 = '  Laranja';
+let fruta2 = 'Banana';
+let fruta3 = 'Abacaxi';
+let fruta4 = 'Manga';
+let fruta5 = 'Morango';
+
+// Length
+console.log(`A quantidade de caracteres da fruta 1 é: ${fruta1.length}, fruta 2 é: ${fruta2.length}, fruta 3 é: ${fruta3.length}, fruta 4 é: ${fruta4.length} e fruta 5 é: ${fruta5.length}`);
+
+// Substring
+console.log(fruta2.substring(0, 3));
+
+// Trim
+console.log(fruta1.trim());
+
+// Replace
+console.log(fruta3.replace('Abacaxi', 'Kiwi'));
+
+// LowerCase
+console.log(fruta3.toLowerCase());
+
+// Listagem das frutas
+
+console.log(`O nome das frutas são: ${fruta1}, ${fruta2}, ${fruta3}, ${fruta4}, ${fruta5}`)
+
+
+
