05993340ca08839e256070194c4bd7eb751dfed8
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-05-18 20:29:09 -0300

    Ex 7

---
+++ workspace/js/exercicios/ex-7/assets/js/main.js
@@ -0,0 +1,40 @@
+const pessoas = [{
+    nome: 'Ana',
+    idade: 17,
+    nota: 8,
+    ano: '2°B',
+}, {
+    nome: 'Bruno',
+    idade: 16,
+    nota: 6,
+    ano: '2°C',
+}, {
+    nome: 'Veronica',
+    idade: 16,
+    nota: 6,
+    ano: '2°C',
+}, {
+    nome: 'Marta',
+    idade: 15,
+    nota: 5,
+    ano: '3°C',
+}, {
+    nome: 'Brenno',
+    idade: 19,
+    nota: 6,
+    ano: '3°C',
+}, {
+    nome: 'Maria',
+    idade: 14,
+    nota: 4,
+    ano: '1°F',
+}];
+
+
+console.log(pessoas);
+
+console.log(pessoas[1]);
+
+console.log(pessoas[5].nota)
+
+console.log(JSON.stringify(pessoas));
