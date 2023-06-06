73aa296119055f413645930a49436d19cce5cf45
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-06-06 18:50:05 -0300

    Ex 15

--- workspace/js/exercicios/ex-15/assets/js/main.js
+++ workspace/js/exercicios/ex-15/assets/js/main.js
@@ -1,57 +1,61 @@
 const pegaNome = () => {
     const senhaRegex = /^(?=.*[!@#$%^&*])(?=.*[0-9])(?=.*[A-Z]).{8,}$/;
-    if (!senhaRegex.test(senha)) {
-        alert('Teste');
-        const nome = document.getElementById('nome').value;
-        const senha = document.getElementById('senha').value;
+    const nome = document.getElementById('nome').value;
+    const senha = document.getElementById('senha').value;
+    const cargoElement = document.getElementById('cargo');
+    const cargo = cargoElement.value;
+    let numeroCargo;
 
-        console.log(`Seu nome é ${nome} e sua senha é ${senha}`);
-    }
-    else {
-        alert('a')
+    if (!senhaRegex.test(senha)) {
+        alert(`Usuário ou senha incorretos!`)
+
+        
+    } else {
+        
+
+        switch (cargo) {
+            case 'analista':
+                numeroCargo = 1;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'estagio':
+                numeroCargo = 2;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'trainee':
+                numeroCargo = 3;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'junior':
+                numeroCargo = 4;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'pleno':
+                numeroCargo = 5;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'senior':
+                numeroCargo = 6;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'lider tecnico':
+                numeroCargo = 7;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+            case 'ceo':
+                numeroCargo = 8;
+                alert(`Olá ${nome}, seu cargo é ${numeroCargo}, vamos te redirecionar para a página desejada`);
+                break;
+
+        }
+        // Redirecionar usuario
+        window.location.href = "https://www.google.com";
     }
 }
-
-// const teste = () => {
-//     const cargoElement = document.getElementById('cargo');
-//     const cargo = cargoElement.value;
-
-//     let nomeCargo;
-
-//     switch (cargo) {
-//         case 'analista':
-//             nomeCargo = 1;
-//             break;
-
-//         case 'estagio':
-//             nomeCargo = 2;
-//             break;
-
-//         case 'trainee':
-//             nomeCargo = 3;
-//             break;
-
-//         case 'junior':
-//             nomeCargo = 4;
-//             break;
-
-//         case 'pleno':
-//             nomeCargo = 5;
-//             break;
-
-//         case 'senior':
-//             nomeCargo = 6;
-//             break;
-
-//         case 'lider tecnico':
-//             nomeCargo = 7;
-//             break;
-
-//         case 'ceo':
-//             nomeCargo = 8;
-//             break;
-
-//     }
-
-//     console.log(nomeCargo);
-// }
